d974e2b04ea0b2647ae8f06cd8782650f965b5a5
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-26 20:58:05 +0500

    Completed Tours Layout

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,11 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import Loading from './Loading'
 import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // Fetching Tours From api
+  const fetchTours = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      // console.log(tours);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  // using useEffect Hook / (componentDidMount in classes)
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  return (loading ? <Loading /> :
+    (<main><Tours tours={tours}/></main>)
+  )
 }
 
 export default App
