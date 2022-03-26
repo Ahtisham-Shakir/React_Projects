dc24041b59d93a481ee7d41fb65a11383963f4c2
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-26 21:11:16 +0500

    Added Toggle Text Functionality

--- 02-tours/src/Tour.jsx
+++ 02-tours/src/Tour.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, price, name }) => {
+    const [readMore, setReadMore] = useState(false);
     return <article className='single-tour'>
         <img src={image} alt="tour" />
         <footer>
@@ -8,7 +9,10 @@ const Tour = ({ id, image, info, price, name }) => {
                 <h4>{name}</h4>
                 <h4 className='tour-price'>${price}</h4>
             </div>
-            <p>{info}</p>
+            <p>
+                {readMore? info : `${info.slice(0,200)}...`}
+                <button onClick={()=>setReadMore(!readMore)}> {readMore?'ShowLess':'ReadMore'} </button>
+                </p>
             <button className='delete-btn'>Not Interested</button>
         </footer>
     </article>;
