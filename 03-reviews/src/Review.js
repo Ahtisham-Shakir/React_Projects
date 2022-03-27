4b569c52acd6a476748b639988d35382fd28304e
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-03-27 21:50:20 +0500

    Added Previous<, Next and Random button functionality

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -5,6 +5,44 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  // Funcion to control index
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    else if (number < 0) {
+      return people.length - 1;
+    }
+    else {
+      return number;
+    }
+  }
+
+  // Function For Next Person
+  const prevPerson = () => {
+    setIndex(index => {
+      const newIndex = index - 1;
+      return checkNumber(newIndex)
+    })
+  }
+
+  // Function For previous Person
+  const nextPerson = () => {
+    setIndex(index => {
+      const newIndex = index + 1;
+      return checkNumber(newIndex)
+    })
+  }
+
+  // Function for random person
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index){
+      randomIndex = checkNumber(index+1);
+    }
+    setIndex(randomIndex);
+  }
   return (
     <article className='review'>
       <div className='img-container'>
@@ -17,15 +55,15 @@ const Review = () => {
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='prev-btn'>
+        <button className='prev-btn' onClick={prevPerson}>
           <FaChevronLeft />
         </button>
 
-        <button className='next-btn'>
-          <FaChevronRight className='prev-btn' />
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
         </button>
 
-        <button className='random-btn'>
+        <button className='random-btn' onClick={randomPerson}>
           Surprise Me
         </button>
       </div>
