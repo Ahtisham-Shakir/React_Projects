f3cf085cbbd670a216530489b9b92e99dbd27238
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-04-14 00:56:51 +0500

    Completed Our Menu Project

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -3,8 +3,36 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+// Dynamically getting categories
+// items file has same category multiple time so.... new Set get only unique category and resolve same occurence to 1
+const allCategories = ['all', ...new Set(items.map(item=>item.category))];
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([...allCategories]);
+
+  // Function to filter items
+  function filterItems(category) {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+
+    const newItems = items.filter(item => item.category === category);
+    setMenuItems(newItems);
+  }
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>Our Menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
